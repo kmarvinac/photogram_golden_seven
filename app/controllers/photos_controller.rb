5b6ddeed95e882d56f0d78d31c5bb5890bd40d26
Kevin Marvinac <kevinmarvinac@users.noreply.github.com>
2016-10-29 18:29:21 -0500

    first set of commits - making everything work - still need to make edit button auto redirect

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,34 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id] })
+  end
+
+  def delete
+    @delete = Photo.find_by({ :id => params[:id] })
+    @delete.destroy
+    redirect_to("/photos")
+  end
+
+  def create_row
+    p = Photo.new
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+
+    redirect_to("/photos")
+  end
+
+  def edit_form
+    @p = Photo.find_by({ :id => params[:id] })
+  end
+
+  def update_row
+    @p = Photo.find_by({ :id => params[:id] })
+    @p.source = params[:new_source]
+    @p.caption = params[:new_caption]
+    @p.save
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,20 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  #routes to create photos
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+  get("/create_photo", { :controller => "photos", :action => "create_row" })
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+
+  #routes to delete photos
+  get("/delete_photo/:id",       { :controller => "photos", :action => "delete" })
+
+  #routes to edit photos
+  get("/photos/:id/edit",       { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
 
 end
